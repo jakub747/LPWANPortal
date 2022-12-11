4b51b23d0a1158a0851ff339b18b24c53eb65a63
Vana Jakub <jakub.vana@comap-control.com>
2022-12-11 15:45:16 +0100

    Prepare responsive navbar.
    Signed-off-by: Vana Jakub

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import "./Home.css"
+
+function Home() {
+    return(
+    <>
+        <div className="home">
+            <h1 className="portal">LPWAN Portal</h1>
+        </div>
+    </>
+    );
+}
+
+export default Home;
