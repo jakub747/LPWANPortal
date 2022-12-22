673b4bdbdbe8f73be7da9cc122b470c1aba7edf7
Vana Jakub <jakub.vana@comap-control.com>
2022-12-22 16:26:17 +0100

    Change of elements structure in login and register pages.
    Signed-off-by: Vana Jakub

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -16,30 +16,18 @@ export default function Login({ loggedIn }) {
         <Header />
         <main>
             <h1>Přihlášení</h1>
-            <div className='main'>
-                <div className='content-box'>
-                    <div>
-                        <input type="text" placeholder="Email" className="input-box" />
-                    </div>
-                    <div className='input-item'>
-                        <input type="password" placeholder="Heslo" className="input-box" />
-                    </div>
-                    <div className='input-item'>
-                        <button className='black-button'>Přihlásit</button>
-                    </div>
-                    <hr className='dividing-line' />
-                    <div class="signin">
-                        <button type="button" class="login-with-google-btn">Sign in with Google</button>
-                        <button type="button" class="login-with-apple-btn">Sign in with Apple</button>
-                    </div>
-                    <div className='footer-form'>
-                        <p>Ještě nemáte účet?</p>
-                        <a href="/register">
-                            <button className='black-button'>Registrovat</button>
-                        </a>
-                    </div>
-                </div>
-            </div>
+            <section className='content-box'>
+                <form action="/sign_in.php" method="post">
+                    <input type="text" placeholder="Email" className="input-box" />
+                    <input type="password" placeholder="Heslo" className="input-box" />
+                    <button className='black-button'>Přihlásit</button>
+                </form>
+                <hr className='dividing-line' />
+                <button type="button" class="login-with-google-btn">Sign in with Google</button>
+                <button type="button" class="login-with-apple-btn">Sign in with Apple</button>
+                <p>Ještě nemáte účet?</p>
+                <a href="/register"><button className='black-button-small'>Registrovat</button></a>
+            </section>
         </main>
     </>
 }
--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -17,39 +17,21 @@ export default function Regstration({ loggedIn }) {
         <Header />
         <main>
             <h1>Registrace</h1>
-            <div className='main'>
-                <div className='content-box'>
-                    <div>
-                        <input type="text" placeholder="Jméno" className="input-box" />
-                    </div>
-                    <div className='input-item'>
-                        <input type="text" placeholder="Příjmení" className="input-box" />
-                    </div>
-                    <div className='input-item'>
-                        <input type="text" placeholder="Email" className="input-box" />
-                    </div>
-                    <div className='input-item'>
-                        <input type="password" placeholder="Heslo" className="input-box" />
-                    </div>
-                    <div className='input-item'>
-                        <input type="password" placeholder="Heslo znovu" className="input-box" />
-                    </div>
-                    <div className='input-item'>
-                        <button className='black-button'>Registrovat</button>
-                    </div>
-                    <hr className='dividing-line' />
-                    <div class="signin">
-                        <button type="button" class="login-with-google-btn">Sign up with Google</button>
-                        <button type="button" class="login-with-apple-btn">Sign up with Apple</button>
-                    </div>
-                    <div className='footer-form'>
-                        <p>Již máte účet?</p>
-                        <a href='/login'>
-                            <button className='black-button'>Přihlásit se</button>
-                        </a>
-                    </div>
-                </div>
-            </div>
+            <section className='content-box'>
+                <form action="/register.php" method="post">
+                    <input type="text" placeholder="Jméno" className="input-box" />
+                    <input type="text" placeholder="Příjmení" className="input-box" />
+                    <input type="text" placeholder="Email" className="input-box" />
+                    <input type="password" placeholder="Heslo" className="input-box" />
+                    <input type="password" placeholder="Heslo znovu" className="input-box" />
+                    <button className='black-button'>Registrovat</button>
+                </form>
+                <hr className='dividing-line' />
+                <button type="button" class="login-with-google-btn">Sign up with Google</button>
+                <button type="button" class="login-with-apple-btn">Sign up with Apple</button>
+                <p>Již máte účet?</p>
+                <a href='/login'><button className='black-button-small'>Přihlásit se</button></a>
+            </section>
         </main>
         <Footer />
     </>
